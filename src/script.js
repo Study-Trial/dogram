b94bbafbf175b4505002b4454e26fe32caab9f55
sorrynogmailauthentication <mike.volchek@gmail.com>
2025-01-31 02:32:50 +0200

    HW#9-updated

--- src/script.js
+++ src/script.js
@@ -1,22 +1,22 @@
 const detailedImage = document.querySelector(".detailed_image");
 const detailedTitle = document.querySelector(".detailedContainer--title");
-const gallery = document.querySelectorAll(".gallery--item .gallery--item_container .gallery--item_image");
-let detailedImageAnimate = document.querySelector("#id_detailed_image");
+const gallery = document.querySelectorAll(".gallery--item_image");
 
 function setDetails(galleryItem) {
   detailedImage.src = galleryItem.getAttribute("src");
   detailedTitle.innerHTML = galleryItem.getAttribute("data-detailed-text");
 }
-function animate() {
-    detailedImageAnimate.classList.remove("animate__backInRight");
-    void detailedImageAnimate.offsetWidth;
-    detailedImageAnimate.classList.add("animate__backInRight");
+function animationOff() {
+    detailedImage.classList.remove("animate__backInRight");
+}
+
+function animationOn() {
+    detailedImage.classList.add("animate__backInRight");
 }
 
 for(let i = 0; i < gallery.length; i++) {
     gallery[i].addEventListener("click", function () {
         setDetails(gallery[i]);
-        animate();
-  });
+        animationOn();
+  });     
 }
-    
