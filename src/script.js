c88bc32bf906d289fab73e97ae9db99bc7dcb31d
sorrynogmailauthentication <mike.volchek@gmail.com>
2025-01-31 02:53:51 +0200

    HW#9-ignores-identical-images

--- src/script.js
+++ src/script.js
@@ -1,10 +1,10 @@
 const detailedImage = document.querySelector(".detailed_image");
 const detailedTitle = document.querySelector(".detailedContainer--title");
-const gallery = document.querySelectorAll(".gallery--item_image");
+const gallery = document.querySelectorAll(".gallery--item");
 
 function setDetails(galleryItem) {
-  detailedImage.src = galleryItem.getAttribute("src");
-  detailedTitle.innerHTML = galleryItem.getAttribute("data-detailed-text");
+  detailedImage.src = galleryItem.querySelector("img").src;
+  detailedTitle.innerHTML = galleryItem.querySelector("img").getAttribute("data-detailed-text");
 }
 function animationOff() {
     detailedImage.classList.remove("animate__backInRight");
@@ -16,7 +16,10 @@ function animationOn() {
 
 for(let i = 0; i < gallery.length; i++) {
     gallery[i].addEventListener("click", function () {
-        setDetails(gallery[i]);
-        animationOn();
+        if(gallery[i].querySelector("img").src == detailedImage.src) {
+        } else {
+            setDetails(gallery[i]);
+            animationOn();
+        }
   });     
 }
